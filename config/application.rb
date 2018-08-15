a4f8264b169631f7f28ce312263f883a15d7237c
KIMHOA <phamhoa041196@gm.comail>
2018-08-15 14:17:35 +0700

    Chapter 4: Rails-flavored Ruby

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,8 +11,8 @@ module LearnRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :en
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
